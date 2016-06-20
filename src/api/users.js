d1b08c11a4dc9f86be0c1e10344c194ed6ae51f8
Frederic Rey <Frederic.Rey.Pro@gmail.com>
2016-06-20 22:32:03 +0100

    - usersRoute was in in the wrong file posts.js. Code was moved to users.js

---
+++ src/api/users.js
@@ -0,0 +1,62 @@
+/**
+ * USERS RESTful API
+ */
+import express from 'express';
+
+const usersRoute = express.Router().route('/users/:user_id');
+
+// Returns all the users
+usersRoute
+  .all((req, res, next) => {
+    // Put here code that is run whatever the verb
+    //
+  })
+  .get(({ user }, res, next) => {
+  // send single user
+    if (user) {
+      res.json(user);
+
+    // Send all the users
+    } else {
+      res.json([
+        {
+          id: 1,
+          fname: 'Albert',
+          lname: 'Gilbert',
+          email: 'agil@mail.uk',
+          password: 'agilPass',
+        },
+        {
+          id: 2,
+          fname: 'Franken',
+          lname: 'Stein',
+          email: 'fste@mail.uk',
+          password: 'fstePass',
+        },
+        {
+          id: 3,
+          fname: 'Franken',
+          lname: 'Stein',
+          email: 'fste@mail.uk',
+          password: 'fstePass',
+        },
+      ]);
+    }
+  })
+  .put(({ user, params }, res, next) => {
+    // TODO: Get user updated info from params
+
+    // TODO: Update user in DB
+    let newUser;
+
+    // TODO: Send updated user
+    res.json(newUser);
+  })
+  .post(({ params }, res, next) => {
+    // TODO: Get new user info from params
+
+    // TODO: Insert new user in DB
+  });
+
+
+export default usersRoute;
